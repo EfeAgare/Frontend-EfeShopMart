2edea624d10f79ac3d69db7f5083627a5cc33471
Efe Agare <knowledgeagare157@gmail.com>
2023-06-12 15:43:20 +0100

    create Apollo server and all required css

--- src/server/apolloClient.js
+++ src/server/apolloClient.js
@@ -1,53 +1,100 @@
 import { ApolloClient, InMemoryCache, ApolloLink, split } from '@apollo/client';
 import { getMainDefinition } from '@apollo/client/utilities';
 import { createUploadLink } from 'apollo-upload-client';
-import { setContext } from "@apollo/client/link/context";
+// import { WebSocketLink } from '@apollo/client/link/ws';
 
 // Create an http link:
-const httpLink = new createUploadLink({
-  uri: 'http://localhost:3000/graphql',
+const httpLink = createUploadLink({
+  uri: 'http://127.0.0.1:3000/graphql',
 });
 
-const middlewareLink = setContext(() => ({
-  headers: {
-    'x-token': localStorage.getItem('token'),
-  },
-}));
+// Create a WebSocket link for subscriptions:
+// const wsLink = new WebSocketLink({
+//   uri: 'ws://127.0.0.1:3000/graphql',
+//   options: {
+//     reconnect: true,
+//   },
+// });
 
 const afterwareLink = new ApolloLink((operation, forward) => {
-  const { headers } = operation.getContext();
+  operation.setContext(({ headers }) => ({
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem('token')}`,
+      ...headers,
+    },
+  }));
 
-  if (headers) {
-    const token = headers.get('x-token');
+  return forward(operation).map(response => {
+    const { response: { headers } } = operation.getContext();
 
-    if (token) {
-      localStorage.setItem('token', token);
+    if (headers) {
+      const token = headers.get('token');
+
+      if (token) {
+        localStorage.setItem('token', token);
+      }
     }
-  }
 
-  return forward(operation);
+    return response;
+  });
 });
 
-const httpLinkWithMiddleware = afterwareLink.concat(
-  middlewareLink.concat(httpLink)
-);
+const additiveLink = afterwareLink.concat(httpLink);
 
-// using the ability to split links, you can send data to each link
-// depending on what kind of operation is being sent
 const link = split(
-  // split based on operation type
+  // Split based on operation type
   ({ query }) => {
     const definition = getMainDefinition(query);
     return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
+      definition.kind === 'OperationDefinition'
+      //  &&
+      // definition.operation === 'subscription'
     );
   },
-  httpLinkWithMiddleware
+  // wsLink,
+  additiveLink
 );
 
-export default new ApolloClient({
+const client = new ApolloClient({
   link,
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          // Add field policies for specific query fields if needed
+          // fetchAllProducts: {
+//             keyArgs: false,
+
+//             merge(existing = [], incoming) {
+//               console.log("existing", existing)
+//               console.log("incoming", incoming)
+//               return [...existing, ...incoming?.products
+// ];
+//             },
+
+            // keyArgs: [],
+            // merge(existing, incoming, { args: { page = 0 } }) {
+            //   // Slicing is necessary because the existing data is
+            //   // immutable, and frozen in development.
+            //   const merged = existing ? existing.slice(0) : [];
+            //   console.log("merge", merged)
+            //   for (let i = 0; i < incoming?.products?.length; ++i) {
+            //     console.log("merge", merged[page + i])
+            //     merged[page + i] = incoming?.products[i];
+            //   }
+            //   return merged;
+            // },
+          // }
+        },
+      },
+    },
+  }),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-and-network',
+      nextFetchPolicy: 'cache-first',
+    },
+  },
 });
 
+export default client;
